e0756cddfa65c47bef5e1467d10798100426893b
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-20 17:52:40 +0530

    done

--- main.ts
+++ main.ts
@@ -2,16 +2,10 @@
 
 const lines: string = require('fs').readFileSync(0, 'utf-8').trim();
 
-const num = Number(lines);
-if (num % 15 === 0) {
-  console.log("FizzBuzz");
+let sum: number = 0;
+
+for (let i = 1; i <= Number(lines); i++){
+  sum += i;
 }
-else if (num % 3 === 0) {
-  console.log("Fizz");
-}
-else if (num % 5 === 0) {
-  console.log("Buzz");
-}
-else {
-  console.log(num);
-}
+
+console.log(sum);
